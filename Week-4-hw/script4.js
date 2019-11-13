9c2e2046f109288fc9a81933503d1925dde00780
Rosalind Killingstad <rosalindkillingstad@Rosalinds-MacBook-Pro.local>
2019-11-12 19:15:31 -0700

    updated, styled quiz

--- Week-4-hw/script4.js
+++ Week-4-hw/script4.js
@@ -15,12 +15,62 @@
 //get value of what the user put, so as to see if its correct-setting a variable to the submission with .value
 //need a timer in header/footer that shows remaining time in interval
 
+var Qnumber = document.getElementById("Qnumber");
+var test = document.getElementById("test");
+var submitButton = document.getElementById("submit");
+var results = document.getElementById("scores");
+var title = 0;
+var output = [];
+var startButton = document.getElementById("start");
+
+
+
+//start button to begin test
+//clicking starts timer and sends to first question
+
+startButton.onClick = function setUpQuiz() {
+
+
+};
+
+
+
+
+
+
 var position = 0,
     test, Qnumber, question, choice, choices, chA, chB, chC, chD, correct = 0;
-var questions = [
-    [],
-    [],
-    [],
-    [],
-    []
+var questions = [{
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "....",
+        choices: ["a", "b", "c", "d"],
+        answer: "b"
+
+    }
 ];
+
+function setUpQuiz() {}
+
+
+
+function displayResults() {}
+
+
+setUpQuiz();
+
+
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+});
