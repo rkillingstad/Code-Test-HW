89e47ee73aee53cd72f5e3e1b640ff5509d65ed1
Rosalind Killingstad <rosalindkillingstad@Rosalinds-MacBook-Pro.local>
2020-02-03 20:59:58 -0700

    ready to go live

--- Week-4-hw/script4.js
+++ Week-4-hw/script4.js
@@ -1,16 +1,9 @@
 //psuedocode
-//need header with title
-//need a start quiz button when first opened
-//need to set variables (so need id's and class names to reference)
-//need a question, followed by 4 possible answers 
 //each answer needs a button next to it that can be clicked and changes when clicked
-//need a submit button--changes with hover 
 //when the submit button is pressed you are taken to next question
 //each question only has a certain amount of time to answer
 //after time remaining, will go to next question (if answered or not)
-//make the questions an array(??)
 //do setTimeOut to give a specific amount of time
-//eventListener for submit eg. submitEL.addEventListener("click", function(event){...})
 //need to prevent default
 //get value of what the user put, so as to see if its correct-setting a variable to the submission with .value
 //need a timer in header/footer that shows remaining time in interval
@@ -19,58 +12,139 @@ var Qnumber = document.getElementById("Qnumber");
 var test = document.getElementById("test");
 var submitButton = document.getElementById("submit");
 var results = document.getElementById("scores");
-var title = 0;
-var output = [];
-var startButton = document.getElementById("start");
+const startButton = document.getElementById("start");
+var display = document.querySelector(".main");
+var chA = document.querySelector("#chA");
+var chB = document.querySelector("#chB");
+var chC = document.querySelector("#chC");
+var chD = document.querySelector("#chD");
+var qTitle = document.querySelector("#question");
+var timer = document.querySelector("#timer");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+var count = 0;
 
+$(document).ready(function() {
 
+    $(".main").hide();
 
-//start button to begin test
-//clicking starts timer and sends to first question
+    $("#start").on("click", function() {
+        $("#start").hide();
+        $(".main").show();
+        quizStart();
+    });
 
-startButton.onClick = function setUpQuiz() {
+    console.log("#start");
 
+    $("#submit").on("click", function() {
+        event.preventDefault();
+        count++;
+        console.log(count);
+        validateAnswer();
+        nextQuestion();
+    })
+});
 
-};
-
-
-
-
-
-
-var position = 0,
-    test, Qnumber, question, choice, choices, chA, chB, chC, chD, correct = 0;
 var questions = [{
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        qTitle: "Commonly used data types DO NOT include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: 2
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        qTitle: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+        answer: 2
     },
 
     {
-        title: "....",
-        choices: ["a", "b", "c", "d"],
-        answer: "b"
+        qTitle: "Which Software Company Developed JavaScript?",
+        choices: ["Oracle", "Sun Microsystmes", "Netscape", "Mozilla"],
+        answer: 3
 
     }
 ];
+console.log(questions);
 
-function setUpQuiz() {}
+var position = 0,
+    test, Qnumber, question, choice, choices, chA, chB, chC, chD, correct = 0;
+var lastQuestion = questions.length - 1;
+var currentQuestion = 0;
+let score = 0;
 
+Qnumber.textContent = "Question " + (currentQuestion + 1) + " of " + questions.length + ":";
+console.log(Qnumber);
 
+function quizStart() {
+    setUpQuiz();
+};
 
-function displayResults() {}
+function setUpQuiz() {
+    var Q = questions[currentQuestion];
+    qTitle.innerHTML = Q.qTitle;
+    chA.innerHTML = Q.choices[0];
+    chB.innerHTML = Q.choices[1];
+    chC.innerHTML = Q.choices[2];
+    chD.innerHTML = Q.choices[3];
+    console.log("Current Q" + currentQuestion);
+};
 
+function nextQuestion() {
+    if (count < questions.length) {
+        var Q = questions[count];
+        qTitle.innerHTML = Q.qTitle;
+        chA.innerHTML = Q.choices[0];
+        chB.innerHTML = Q.choices[1];
+        chC.innerHTML = Q.choices[2];
+        chD.innerHTML = Q.choices[3];
 
-setUpQuiz();
+        console.log("Current Q" + currentQuestion);
 
+        Qnumber.textContent = "Question " + (count + 1) + " of " + questions.length + ":";
+        console.log(Qnumber);
+    } else {
+        console.log("end of quiz!");
+        displayResults();
+    }
+};
 
+function validateAnswer() {
+    var radioValue = $('input[name=choices]:checked').val();
+    console.log("Selected value: " + radioValue);
+    var correctAnswer = questions[count - 1].answer;
+    console.log("Correct answer: " + correctAnswer);
 
-submitButton.addEventListener("click", function(event) {
-    event.preventDefault();
+    if (radioValue == correctAnswer) {
+        score++;
+        console.log("Score: " + score);
+        alert("That's Correct!");
+    } else {
+        console.log("wrong answer");
+        alert("Wrong answer!");
+    }
 
-});
+    scoreDiv.textContent = "Your Score is " + score + " out of " + questions.length + ".";
+};
+
+var qTime = 60;
+startButton.onclick = function qTimer() {
+    qTime--;
+    if (qTime > 0) {
+        setTimeout(qTimer, 1000);
+        document.getElementById("timer").innerHTML = qTime + " seconds remaining";
+    } else {
+        console.log("Time's up!");
+        alert("Time's up!");
+        displayResults();
+    }
+    console.log(qTime);
+};
+
+function displayResults() {
+    $("#timer").hide();
+    $("#submit").hide();
+    $("#choices").hide();
+    $("#question").hide();
+    Qnumber.textContent = "Final Score: ";
+    scoreDiv.textContent = "Your final Score is " + score + " out of " + questions.length + ".";
+
+};
